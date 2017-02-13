116b853b10674aef5579265990f2323bcd46fd13
Owen Klaiss <oklaiss@smu.edu>
2017-02-13 16:59:53 -0600

    asset precomline flag

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module DoxBox
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Needed for Heroku so serve assets
+    config.serve_static_assets = true
   end
 end
