a5326bcce4a91b537607e43f10600aa0fea2afb3
Owen Klaiss <oklaiss@smu.edu>
2017-03-24 13:17:13 -0500

    sanitize params for user signup and update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-  # before_action :authenticate_user!
+    # Prevent CSRF attacks by raising an exception.
+    # For APIs, you may want to use :null_session instead.
+    protect_from_forgery with: :exception
+    # before_action :authenticate_user!
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+	def configure_permitted_parameters
+	  devise_parameter_sanitizer.permit(:sign_in, keys: [:name, :email])
+	  devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation]) 
+	  devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password, :password_confirmation, :current_password])
+	end
 end
